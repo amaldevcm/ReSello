bd7bcfd7fba07fc32d2fdcbc24fefe88e30e02ed
Amal Dev Cheepra Mailapravan <43089089+amaldevcm@users.noreply.github.com>
2025-07-02 13:22:42 -0400

    Update mongoose.js

--- Backend/database/mongoose.js
+++ Backend/database/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = encodeURI('mongodb+srv://developer:DeyTrkdORpI3Tc7e@cluster0.d41ro.mongodb.net/Ecommerce');
+const uri = encodeURI('your db url');
 mongoose.connect(uri)
         .then(() => {
             console.log('Database connected');
